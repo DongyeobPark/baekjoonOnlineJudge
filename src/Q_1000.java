af04481f140f2b0ec59a3f1b43813e503696b3fe
DongYeob.Park <funny_blue@naver.com>
2016-06-20 21:55:46 +0900

    main 프로젝트 생성 및 1000번 문제 작성

---
+++ src/Q_1000.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputValue = sc.nextLine();
+        String[] splitInputValue = inputValue.split(" ");
+        System.out.println(Integer.parseInt(splitInputValue[0]) + Integer.parseInt(splitInputValue[1]));
+    }
+}
