fe19299144fd00208382fe94a5a8e8a6455c271e
DongYeob.Park <funny_blue@naver.com>
2016-07-04 07:56:53 +0900

    [1003] 피보나치를 사용하지 않

--- src/Q_1003.java
+++ src/Q_1003.java
@@ -3,24 +3,27 @@ import java.util.Scanner;
 public class Q_1003 {
     public static void main(String[] args) {
         Scanner scInput = new Scanner(System.in);
-        int loopCount = Integer.parseInt(scInput.nextLine());
+        long loopCount = Integer.parseInt(scInput.nextLine());
         while (loopCount-- > 0) {
-            int[] answerList = fibonacci(scInput.nextInt(), 0, 0);
-            System.out.println(answerList[1] + " " + answerList[2]);
-        }
-    }
-
-    public static int[] fibonacci(int n, int zeroCount, int oneCount) {
-        if (n == 0) {
-            // System.out.println("0");
-            return (new int[] {0, ++zeroCount, oneCount});
-        } else if (n == 1) {
-            // System.out.println("1");
-            return (new int[] {1, zeroCount, ++oneCount});
-        } else {
-            int[] fibo1 = fibonacci(n-1, zeroCount, oneCount);
-            int[] fibo2 = fibonacci(n-2, zeroCount, oneCount);
-            return (new int[] {fibo1[0] + fibo2[0], (fibo1[1] + fibo2[1]), (fibo1[2] + fibo2[2])});
+            long inputValue = Integer.parseInt(scInput.nextLine());
+            if (inputValue == 0) {
+                System.out.println("0 1");
+            } else if (inputValue == 1) {
+                System.out.println("1 0");
+            } else if (inputValue == 2) {
+                System.out.println("1 1");
+            } else {
+                long oneCount = 1;
+                long zeroCount = 1;
+                long beforeOneCount = 0;
+                while (inputValue-- >= 3) {
+                    zeroCount = zeroCount + oneCount;
+                    long countTemp = oneCount;
+                    oneCount = oneCount + beforeOneCount;
+                    beforeOneCount = countTemp;
+                }
+                System.out.println(oneCount + " " + zeroCount);
+            }
         }
     }
 }
