a8da65a8a9304a9398d5e1ae58e795330a0f210c
DongYeob.Park <funny_blue@naver.com>
2016-07-04 08:23:15 +0900

    [1003] 피보나치를 사용하지 않으면서, 자료형을 long에서 BigInteger로 바꿔봄. 그런데 통과 못함.

--- src/Q_1003.java
+++ src/Q_1003.java
@@ -1,26 +1,28 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Q_1003 {
     public static void main(String[] args) {
         Scanner scInput = new Scanner(System.in);
-        long loopCount = Integer.parseInt(scInput.nextLine());
+        int loopCount = Integer.parseInt(scInput.nextLine());
         while (loopCount-- > 0) {
-            long inputValue = Integer.parseInt(scInput.nextLine());
-            if (inputValue == 0) {
+            BigInteger inputValue = new BigInteger(scInput.nextLine());
+            if (inputValue.compareTo(new BigInteger("0")) == 0) {
                 System.out.println("0 1");
-            } else if (inputValue == 1) {
+            } else if (inputValue.compareTo(new BigInteger("1")) == 0) {
                 System.out.println("1 0");
-            } else if (inputValue == 2) {
+            } else if (inputValue.compareTo(new BigInteger("2")) == 0) {
                 System.out.println("1 1");
             } else {
-                long oneCount = 1;
-                long zeroCount = 1;
-                long beforeOneCount = 0;
-                while (inputValue-- >= 3) {
-                    zeroCount = zeroCount + oneCount;
-                    long countTemp = oneCount;
-                    oneCount = oneCount + beforeOneCount;
+                BigInteger oneCount = new BigInteger("1");
+                BigInteger zeroCount = new BigInteger("1");
+                BigInteger beforeOneCount = new BigInteger("0");
+                while (inputValue.compareTo(new BigInteger("2")) == 1) {
+                    zeroCount = zeroCount.add(oneCount);
+                    BigInteger countTemp = oneCount;
+                    oneCount = oneCount.add(beforeOneCount);
                     beforeOneCount = countTemp;
+                    inputValue = inputValue.subtract(BigInteger.valueOf(1));
                 }
                 System.out.println(oneCount + " " + zeroCount);
             }
